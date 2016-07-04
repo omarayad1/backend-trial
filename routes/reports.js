23578c7698bdc22c8a794dcf7485d62aeaf42a69
Omar H. Ayad <omarayad1@gmail.com>
2016-07-04 20:07:46 +0200

    limit allowed months

--- routes/reports.js
+++ routes/reports.js
@@ -8,8 +8,10 @@ var db = new sqlite3.Database(dbPath);
 
 var router = express.Router();
 
+var allowed_months = ['3','9','18']
+
 router.get('/', function(req, res, next) {
-  if (req.query.months && req.query.commission){
+  if (req.query.months && req.query.commission && allowed_months.indexOf(req.query.months)+1){
     db.all(sql_queries.fmt_sql(req.query.months), function(err, rows){
       res.render('report/report', { data: rows, commission: req.query.commission, months: req.query.months});
     });
